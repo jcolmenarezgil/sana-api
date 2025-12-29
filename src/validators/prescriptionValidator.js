801e7df1a4d64e9ff5e23d1c4f2780f67e117e1f
Jose David Colmenarez Gil <jcolmenarezgil@byupathway.edu>
2025-12-29 01:42:22 -0400

    medications err fix on Validator

--- src/validators/prescriptionValidator.js
+++ src/validators/prescriptionValidator.js
@@ -3,7 +3,7 @@ import Joi from 'joi';
 export const prescriptionSchema = Joi.object({
     patient_id: Joi.string().alphanum().length(24).required(),
     related_procedures: Joi.array().items(Joi.string().alphanum().length(24)),
-    medications: Joi.array().items(Joi.object({
+    medication: Joi.array().items(Joi.object({
         medicine_id: Joi.string().alphanum().length(24).required(),
         dosage_form: Joi.string().required(),
         schedule: Joi.string().required(),
